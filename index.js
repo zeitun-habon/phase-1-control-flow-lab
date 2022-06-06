a6841164c88677772c07ef770a7f815c198420cc
zeitun-adan <zeitun.adan@student.moringaschool.com>
2022-06-06 23:23:23 +0300

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride, distance, charges){
   // Write your code here!
+  if (ride<=400) {
+     return 'This one is on me!';
+  } 
+
+ 
+   else if (distance>2000) {
+      return 'No can do.'
+      if(charge=30){
+        return 'I will gladly take your thirty bucks.'
+      }
+      else{
+        return 'No can do.'
+      }
+    }
 }
+
 
 function ternaryCheckCity(){
   // Write your code here!
